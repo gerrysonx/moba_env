42eac1d5ed1d455ea6011be8c98fc9292988e395
gerrysonx <1755914811@qq.com>
2019-10-29 16:50:53 +0800

    modify ppo.py

--- gym_moba/gym_moba/__init__.py
+++ gym_moba/gym_moba/__init__.py
@@ -1,7 +1,7 @@
 from gym.envs.registration import register
 
 register(
-	id='moba-v0',
+	id='moba2-v0',
 	entry_point='gym_moba.envs:MobaEnv',
 )
 
--- gym_moba/setup.py
+++ gym_moba/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(
-	name = 'gym_moba',
+	name = 'gym_moba2',
 	version = '0.0.1',
 	install_requires = ['gym']
 )
