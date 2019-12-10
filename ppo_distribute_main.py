d0d5e7db5c2199fe99c1ab988cecdcda31b6d6a3
PLUS_LINKID <1178166144@qq.com>
2019-12-10 18:05:21 +0800

    modify distribute

--- ppo_distribute_main.py
+++ ppo_distribute_main.py
@@ -2,19 +2,22 @@
 import sys, os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
-from subprocess import call
+import subprocess
 
 
 if __name__=='__main__':
     root_folder = os.path.split(os.path.abspath(__file__))[0]
 
-    horizon_total = 1024 * 512
+    horizon_total = 8192 * 64
     horizon_per_worker = 8192 * 4
-    worker_count = horizon_total / horizon_per_worker
+    worker_count = horizon_total // horizon_per_worker
 
     train_full_path = '{}/ppo_distribute_train.py'.format(root_folder)
     generate_full_path = '{}/ppo_distribute_generate_data.py'.format(root_folder)
+    my_env = os.environ.copy()
+    my_env['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
-    call(["python", train_full_path, '{}'.format(worker_count)])
+    subprocess.Popen(['python', train_full_path, '{}'.format(worker_count)], env=my_env)
+
     for i in range(worker_count):
-        call(["python", generate_full_path, '{}'.format(horizon_per_worker)])
+        subprocess.Popen(['python', generate_full_path, '{}'.format(horizon_per_worker)], env=my_env)
